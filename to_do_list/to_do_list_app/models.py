7fe83299726db9decfaaaea033ca1f5cbaf29b98
Arek <arkadiusz.gluchowicz@gmail.com>
2019-09-02 22:33:05 +0200

    Models - clean up magic numbers

--- to_do_list/to_do_list_app/models.py
+++ to_do_list/to_do_list_app/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+FINISHED_TASK = 2
+NEW_TASK = 1
 STATUS = (
-    ('1', "New"),
-    ('2', "Done")
+    (NEW_TASK, "New"),
+    (FINISHED_TASK, "Done")
 )
+
 
 class Task(models.Model):
     name = models.CharField(max_length=255)
-    status = models.CharField(max_length=1, choices=STATUS, default=1)
+    status = models.SmallIntegerField(choices=STATUS, default=NEW_TASK)
     deadline_date = models.DateField()
     description = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
 
 
 
